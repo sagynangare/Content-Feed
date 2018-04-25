4009f775d93607647a838b42e612cd995a46ddf4
sagynangare <sagynangare@github.com>
2018-04-25 18:07:56 +0530

    added content files

---
+++ content_api/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from content_api.models import Item
+admin.site.register(Item)
+# Register your models here.
---
+++ contentfeed/urls.py
@@ -0,0 +1,23 @@
+"""from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+"""
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.conf import settings
+from rest_framework import routers
+
+from content_api.views import ItemViewSet
+
+router = routers.DefaultRouter()
+router.register(r'item', ItemViewSet, base_name='item')
+
+urlpatterns = [
+    url(r'^api/', include(router.urls)),
+    url(r'^admin/', admin.site.urls),
+]# + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
